c46829858bed4fbf3668a7c0f10924ede117e4cd
shahjalalrafid-dev <shahjalalrafid201694@gmail.com>
2026-09-24 15:57:49 +0600

    Feat: Dynamically Data Fetched

---
+++ src/app/loading.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const loading = () => {
+    return (
+        <div className='text-center'>
+            <span className="loading loading-ring loading-xs"></span>
+            <span className="loading loading-ring loading-sm"></span>
+            <span className="loading loading-ring loading-md"></span>
+            <span className="loading loading-ring loading-lg"></span>
+            <span className="loading loading-ring loading-xl"></span>
+
+
+        </div>
+    )
+}
+
+export default loading
