d419978d71dac42366d5dcf3521458cebe70c657
shahjalalrafid-dev <shahjalalrafid201694@gmail.com>
2026-09-26 14:19:04 +0600

    Project Deployment Completed and Footer part Responsive

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -4,28 +4,28 @@ import FooterImage from '@/assets/logo.png'
 import Image from 'next/image'
 
 const Footer = () => {
-  return (
-    <footer className='bg-maintheme opacity-95'>
-        <section className='container mx-auto px-5 pt-25 pb-8'>
-            <div className='flex justify-between items-center'>
-                    <Link href='/' >
-
-                        <div className='flex gap-2.5 items-center'>
+    return (
+        <footer className='bg-maintheme opacity-95'>
+            <section className='container mx-auto px-5 pt-25 pb-8'>
+                <div className='flex flex-col lg:flex-row justify-between items-center gap-4 py-6 text-center lg:text-left'>
+                    <Link href='/'>
+                        <div className='flex gap-2.5 items-center justify-center lg:justify-start'>
                             <div>
                                 <Image width={28} height={28} src={FooterImage} alt='Logo Image' />
                             </div>
                             <h6 className='font-oswald font-extrabold text-lg text-white'>FITLOG</h6>
                         </div>
-
                     </Link>
                     <div>
-                        <p className='text-offwhite font-inter text-[12px] font-medium'>© 2026 FitLog — Workout Library. Train hard, log honest.</p>
+                        <p className='text-offwhite font-inter text-[12px] font-medium'>
+                            © 2026 FitLog — Workout Library. Train hard, log honest.
+                        </p>
                     </div>
-            </div>
+                </div>
 
-        </section>
-    </footer>
-  )
+            </section>
+        </footer>
+    )
 }
 
 export default Footer
